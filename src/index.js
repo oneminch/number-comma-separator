daa100d89206357bdaaec84b231389b39b68c4ad
Dawit <27751688+oneminch@users.noreply.github.com>
2020-09-01 19:37:00 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const numSeparator = num => {
+  // Input validation
+  if (isNaN(num)) return "Please enter a number!";
+
+  let decimal = "",
+    numStr = num.toString();
+
+  if (num < 1000) {
+    return numStr;
+  }
+
+  // Check if number is a floating point
+  if (numStr.indexOf(".") > -1) {
+    decimal = `.${numStr.split(".")[1]}`;
+    numStr = numStr.split(".")[0];
+  }
+
+  for (let i = numStr.length; i >= 0; i -= 3) {
+    if (i < 4) break;
+
+    numStr = `${numStr.substring(0, i - 3)},${numStr.substring(
+      i - 3,
+      numStr.length
+    )}`;
+  }
+  return decimal ? numStr.concat(decimal) : numStr;
+};
+
+/* ------------------ User Interface --------------------------- */
+
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+
+input.addEventListener("keyup", e => {
+  e.preventDefault();
+  output.value = numSeparator(e.target.value);
+});
